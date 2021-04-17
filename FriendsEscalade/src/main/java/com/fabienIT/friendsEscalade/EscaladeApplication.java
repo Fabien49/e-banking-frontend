2f9057f4d8a69afb875d7d719213ddf3e917d951
Fabien49 <fabien.chapeau@gmail.com>
2021-04-17 13:38:25 +0200

    grosse maj

---
+++ FriendsEscalade/src/main/java/com/fabienIT/friendsEscalade/EscaladeApplication.java
@@ -0,0 +1,17 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class EscaladeApplication {
+	
+	private static final Logger logger = LoggerFactory.getLogger(EscaladeApplication.class);
+
+	public static void main(String[] args) {
+		SpringApplication.run(EscaladeApplication.class, args);
+		logger.info("Start Les Amis de l'Escalade Application ...");
+	}
+
+}
