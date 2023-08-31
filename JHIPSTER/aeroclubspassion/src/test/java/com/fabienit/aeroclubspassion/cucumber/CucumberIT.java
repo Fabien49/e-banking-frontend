5063ff64cad1451d6a4e0462086787e8734ed663
Fabien49 <fabien.chapeau@gmail.com>
2023-08-31 18:03:01 +0200

    Initial version of aeroclubspassion generated by generator-jhipster@7.9.0

---
+++ JHIPSTER/aeroclubspassion/src/test/java/com/fabienit/aeroclubspassion/cucumber/CucumberIT.java
@@ -0,0 +1,8 @@
+package com.fabienit.aeroclubspassion.cucumber;
+
+import com.fabienit.aeroclubspassion.IntegrationTest;
+import io.cucumber.junit.platform.engine.Cucumber;
+
+@Cucumber
+@IntegrationTest
+class CucumberIT {}
